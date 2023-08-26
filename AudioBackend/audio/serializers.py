6e2e9d39c57450519a70b00f69d24477f13ddf10
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-26 20:40:03 +0530

    initial comment added

--- AudioBackend/audio/serializers.py
+++ AudioBackend/audio/serializers.py
@@ -4,11 +4,11 @@ import mutagen   # importing mutagen to get the size of a audio file
 
 class SongSerializer(ModelSerializer):
     def create(self, validated_data):
-        validated_data['audio']=self.context['request'].FILES['audio']
-        validated_data['name']=self.context['request'].FILES['audio'].name
-        validated_data['size']=round(self.context['request'].FILES['audio'].size/1048576,2)
-        validated_data['extension']=self.context['request'].FILES['audio'].name.split('.')[-1]
-        validated_data['duration']=round(mutagen.File(self.context['request'].FILES['audio']).info.length/60,2)
+        validated_data['audio']=self.context['request'].FILES['audio']  # saving audio in audio field of dictionary
+        validated_data['name']=self.context['request'].FILES['audio'].name   # getting name by name attribute and saving in name variable in dictionary
+        validated_data['size']=round(self.context['request'].FILES['audio'].size/1048576,2) # getting and saving size in size field in dictionary
+        validated_data['extension']=self.context['request'].FILES['audio'].name.split('.')[-1] # saving extension in dictionary
+        validated_data['duration']=round(mutagen.File(self.context['request'].FILES['audio']).info.length/60,2) # getting duration with then help of mutagen library
         return songs.objects.create(**validated_data)
     class Meta:
         model=songs
