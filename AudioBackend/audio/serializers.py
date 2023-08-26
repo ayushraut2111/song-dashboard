17992f617603da41f3eb9e43e7510270e0b2e641
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-26 20:16:49 +0530

    all field fixed in serializer to get saved in database

--- AudioBackend/audio/serializers.py
+++ AudioBackend/audio/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework.serializers import ModelSerializer
 from .models import songs
+import mutagen   # importing mutagen to get the size of a audio file
 
 class SongSerializer(ModelSerializer):
     def create(self, validated_data):
         validated_data['audio']=self.context['request'].FILES['audio']
-        # validated_data['name']=self.context['request'].FILES['audio'].name
+        validated_data['name']=self.context['request'].FILES['audio'].name
+        validated_data['size']=round(self.context['request'].FILES['audio'].size/1048576,2)
+        validated_data['extension']=self.context['request'].FILES['audio'].name.split('.')[-1]
+        validated_data['duration']=round(mutagen.File(self.context['request'].FILES['audio']).info.length/60,2)
         return songs.objects.create(**validated_data)
     class Meta:
         model=songs
--- AudioBackend/audio/views.py
+++ AudioBackend/audio/views.py
@@ -3,14 +3,19 @@ from .models import songs
 from .serializers import SongSerializer
 from rest_framework.response import Response
 from mutagen.wave import WAVE
+import mutagen  # using mutagen to get the size of a audio file
 
 class song_api(ModelViewSet):
     queryset=songs.objects.all()
     serializer_class=SongSerializer
 
     def create(self, request, *args, **kwargs):
-        print(request.FILES['audio'].content_type)
-        ser=SongSerializer(data=request.data,context={'request':request})
+        ser=SongSerializer(data=request.data,context={'request':request})  # in serializer sending request with the help of context
         if ser.is_valid():
             ser.save()
-        return Response({"msg":"created"})
+        if round(mutagen.File(request.FILES['audio']).info.length/60,2)>10:  # checking if the length of audio file is greater than 10 minutes or not
+            return Response({"msg":"Duration Exceeds"})
+        else:
+            return Response({"msg":""})
+
+    
