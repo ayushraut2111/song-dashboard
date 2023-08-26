1cc24438923e28fe65a79cd677a07abe3d5de0bf
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-26 23:26:54 +0530

    frontend ready with all the working and comments

--- AudioBackend/audio/views.py
+++ AudioBackend/audio/views.py
@@ -4,17 +4,19 @@ from .serializers import SongSerializer
 from rest_framework.response import Response
 import mutagen  # using mutagen to get the size of a audio file
 
+
 class song_api(ModelViewSet):
-    queryset=songs.objects.all()
-    serializer_class=SongSerializer
+    queryset = songs.objects.all()
+    serializer_class = SongSerializer
 
     def create(self, request, *args, **kwargs):
-        ser=SongSerializer(data=request.data,context={'request':request})  # in serializer sending request with the help of context
+        # in serializer sending request with the help of context
+        ser = SongSerializer(data=request.data, context={'request': request})
+        print(request.FILES)
         if ser.is_valid():
             ser.save()
-        if round(mutagen.File(request.FILES['audio']).info.length/60,2)>10:  # checking if the length of audio file is greater than 10 minutes or not
-            return Response({"msg":"Duration Exceeds"})
+        # checking if the length of audio file is greater than 10 minutes or not
+        if round(mutagen.File(request.FILES['audio']).info.length/60, 2) > 10:
+            return Response({"msg": "Duration Exceeds"})
         else:
-            return Response({"msg":""})
-
-    
+            return Response({"msg": ""})
--- audiofrontend/src/App.js
+++ audiofrontend/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Audio Dashboard</h1>
+      <Audio/>
     </div>
   );
 }
+
+function Audio()
+{
+  const [aud,setAud]=useState('')
+  const [msg,setMsg]=useState('')
+  console.log(aud)
+  console.log(msg)
+let url="http://127.0.0.1:8000/song/"
+{/* this is a post function and with the help of this function we will post the data to the database */}
+
+let post=async()=>{
+  let formdata = new FormData();
+  formdata.append('audio',aud)
+  console.log(formdata)
+  let x=await fetch(url,{
+    method:"post",
+    body:formdata,
+  })
+  let y= await x.json()
+  setMsg(y)
+{/* after snding audio file to the database we are fetching the updated data from the database */}
+  get()      
+}
+
+let fn=(e)=>{
+  e.preventDefault();   {/* this function is calling post function for sending audio to the database */}
+  post();
+}
+
+const [dt,setDt]=useState([])
+{/* get function is used to fetch all the audio and their metadata from database */}
+const get=async ()=>{          
+    let f=await fetch(url)
+    let data=await f.json()
+{/* after fetching all the data from database it is saving the data in dt variable with the help of setDt hook */}
+    setDt(data)    
+
+  }
+  console.log(dt)
+
+useEffect(()=>{get()},[])
+  return(
+    <>
+    <h1>hello</h1>
+    <div className="frm">
+      {/* using form to take input audio file from user */}
+      <form >             
+        <input type="file" onChange={(e)=>setAud(e.target.files[0])} />    {/* as soon as we upload audio file it saves in aud variable with the help of usestate */}
+        <button type='button' onClick={fn}>Add</button>     {/* when we submit the form it is redirecting to function fn */}
+      </form>
+      {
+        dt.map((data)=>{
+          const {name,date,duration,size,extension,audio}=data
+          return(
+            <>
+            <h3>{name}---{date}--{duration}---{size}---{extension}</h3>
+            <audio src={audio} controls/>
+            </>
+          )
+        })
+      }
+    </div>
+    </>
+  );
+}
+
 
 export default App;
